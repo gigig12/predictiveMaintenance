8363926adce61fc7ded351800986de14710fb925
gigig12 <gigig@tutamail.com>
2024-07-26 18:33:06 +0300

    add new helper functions

---
+++ RoundTable/RoundTable/Modules/InfoModule/UI/InfoModuleViewBinding.swift
@@ -0,0 +1,13 @@
+//  
+//  InfoModuleViewBinding.swift
+//  RoundTable
+//
+//  Created by Stan Potemkin on 11.04.2022.
+//
+
+import Foundation
+import SwiftUI
+
+final class InfoModuleViewBinding: ObservableObject {
+    @Published var counter = Int(0)
+}
