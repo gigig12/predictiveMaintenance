488db6f8f53fa5c28249fc4bc14502ad599c6f1d
gigig12 <gigig@tutamail.com>
2024-07-27 18:33:06 +0300

    improve logging

---
+++ RoundTable/RoundTable/Modules/InfoModule/Logic/InfoModuleState.swift
@@ -0,0 +1,12 @@
+//  
+//  InfoModuleState.swift
+//  RoundTable
+//
+//  Created by Stan Potemkin on 11.04.2022.
+//
+
+import Foundation
+
+final class InfoModuleState {
+    var counter = Int(0)
+}
